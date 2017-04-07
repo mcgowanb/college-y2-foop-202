849535c6d513934cb7fdc1b5124edbcfe4eb383d
Brian McGowan <mcgowan.b@gmail.com>
2017-04-07 01:08:34 +0100

    beginning work on CA2

---
+++ CA-2/CA-2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CA_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CarRentalEntities db;
+        public MainWindow()
+        {
+            db = new CarRentalEntities();
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from ct in db.CarTypes
+                        select ct;
+
+            cbxCarType.ItemsSource = query.ToList();
+        }
+    }
+}
